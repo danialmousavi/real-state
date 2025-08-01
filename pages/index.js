cc702474a9a1f7ef272cd857e58ac0b1b71e83ee
danialmousavi <danialmoosavi69@gmail.com>
2025-08-01 12:57:59 +0330

    complete home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Features from '@/components/templates/index/Features'
+import Story from '@/components/templates/index/Story'
 import React from 'react'
+import Homes from './homes'
+import HomesIndex from '@/components/templates/index/HomesIndex'
+import Gallery from '@/components/templates/index/Gallery'
 
 export default function index() {
   return (
-    <div>index page</div>
+   <>
+   <Features/>
+   <Story/>
+   <HomesIndex/>
+   <Gallery/>
+   </>
   )
 }
